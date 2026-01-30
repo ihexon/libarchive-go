8317c87776bea38461348b6a952c272512e58e80
CAT <14349453+ihexon@users.noreply.github.com>
2026-01-30 23:06:04 +0800

    test(ci): add SetReader test (#2)

--- bsdtar_test.go
+++ bsdtar_test.go
@@ -1,9 +1,14 @@
 package libarchive_go
 
 import (
+	"bytes"
+	_ "embed"
 	"os"
 	"testing"
 )
+
+//go:embed raw-storages.tar
+var rawstorages []byte
 
 func TestModX(t *testing.T) {
 	ShowVersion()
@@ -15,9 +20,18 @@ func TestModX(t *testing.T) {
 		ModeX(); err != nil {
 		t.Errorf("ModeX failed: %v", err)
 	}
+	_ = os.Remove("userdata-storage.raw")
+	_ = os.Remove("container-storage.raw")
 
-	defer func() {
-		_ = os.Remove("userdata-storage.raw")
-		_ = os.Remove("container-storage.raw")
-	}()
+	if err := NewArchiver().SetReader(bytes.NewBuffer(rawstorages)).SetVerbose(1).
+		SetSparse(true).
+		SetFastRead(true).
+		WithPattern("container-storage.raw").
+		WithPattern("userdata-storage.raw").
+		ModeX(); err != nil {
+		t.Errorf("ModeX failed: %v", err)
+	}
+
+	_ = os.Remove("userdata-storage.raw")
+	_ = os.Remove("container-storage.raw")
 }
